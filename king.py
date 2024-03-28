d62320d97f287e8ed6dd235761dcb0565567d2a8
Chonapatcc <Chonapatcz@gmail.com>
2024-03-28 18:15:12 +0700

    1

--- king.py
+++ king.py
@@ -1,13 +1,25 @@
+
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-start =int(input())
-stop = int(input())
+word = input().split(" to ")
+start,stop = int(word[0]),int(word[1])
 
 
-for i in range(start , stop+1):
-    ch =1
-    for prime in primes:
-        if(i%prime==0):
+for i in range(start ,stop+1):
+    cal1 = (i-1)/2
+    cal2 = (i+1)/2
+    #print(cal1,int(cal1),cal2,int(cal2))
+    #break;
+    cal = int(i**(1/2))
+    ch=1;
+    for y in primes:
+        if i%y==0:
             ch=0
-    if ch :
-        print(i)
+            break
+    if(ch):
+        for y in range(98,cal+1):
+            if i%y==0:
+                ch=0
+                break
+    if(ch):
+        print(i,end=" ")
